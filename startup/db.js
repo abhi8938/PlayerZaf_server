5b32a571f71ed123f8e9645d1e3f2b57102e2202
Abhishek Singh <abhishek8938@gmail.com>
2019-02-25 17:05:28 +0530

    7th

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,7 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// module.exports = function () {
-// mongoose.connect('')
-// .then(() => console.log('connected to mongodb....'))
-// .catch(err => console.error('could not connect', err));
-// }
+module.exports = function () {
+mongoose.connect('mongodb+srv://Abhishek:gotranks@cluster0-uo8qi.mongodb.net/test?retryWrites=true')
+.then(() => console.log('connected to mongodb....'))
+.catch(err => console.error('could not connect', err));
+}
