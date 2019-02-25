5cc4597ea216b13e743e1de72f97449611f67520
Abhishek Singh <abhishek8938@gmail.com>
2019-02-25 16:50:49 +0530

    5

--- startup/routes.js
+++ startup/routes.js
@@ -4,7 +4,7 @@ const matchDetails = require('../routes/matchDetails');
 const participants = require('../routes/participants');
 const orders = require('../routes/orders');
 const matches = require('../routes/matches');
-const clients = require('../routes/clients');
+// const clients = require('../routes/clients');
 const auth = require('../routes/auth');
 const result = require('../routes/results');
 const paytm = require('../routes/paytm/paytm.routes')
@@ -15,7 +15,7 @@ module.exports = function(app) {
 app.use('/api/matches', matches);
 app.use('/api/participants', participants);
 app.use('/api/orders', orders);
-app.use('/api/clients', clients);
+// app.use('/api/clients', clients);
 app.use('/api/auth', auth);
 app.use('/api/results', result);
 app.use('/paytm', paytm);
