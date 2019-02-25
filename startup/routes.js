4513a31a1573de7367f164184e403699e1bc4363
Abhishek Singh <abhishek8938@gmail.com>
2019-02-25 18:38:10 +0530

    8th

--- startup/routes.js
+++ startup/routes.js
@@ -1,6 +1,6 @@
 // const error = require('../middleWare/error');
 const express = require('express');
-const clients = require('../routes/clients');
+const clients = require('../routes/users');
 const matchDetails = require('../routes/matchDetails');
 const participants = require('../routes/participants');
 const orders = require('../routes/orders');
@@ -15,7 +15,7 @@ module.exports = function(app) {
 app.use('/api/matches', matches);
 app.use('/api/participants', participants);
 app.use('/api/orders', orders);
-app.use('/api/clients', clients);
+app.use('/api/users', clients);
 app.use('/api/auth', auth);
 app.use('/api/results', result);
 app.use('/paytm', paytm);
