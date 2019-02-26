04fd0982b8c9a4167a7c4329c7f022f68688b381
Abhishek Singh <abhishek8938@gmail.com>
2019-02-27 03:12:13 +0530

    final check

--- startup/db.js
+++ startup/db.js
@@ -4,8 +4,7 @@ module.exports = function () {
 mongoose.connect('mongodb+srv://Abhishek:abhishek@cluster1-h7cgu.mongodb.net/pubgc-db?retryWrites=true')
 .then(() => console.log('connected to mongodb....'))
 .catch(err => console.error('could not connect', err));
-
+}
 // mongoose.connect('mongodb://localhost/pubgC-db')
 //         .then(() => console.log('running'))
 //         .catch(err => console.log('not running'))
-// }
