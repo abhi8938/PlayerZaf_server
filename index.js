e2b377608f924de364c99de6b300ef5d39163862
Abhishek Singh <abhishek8938@gmail.com>
2019-02-27 03:46:41 +0530

    final check3

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 
 require('express-async-errors');
 const startupDebugger = require('debug')('app:startup');
-const express = require('express')
-, redirect = require('express-redirect');
+const express = require('express');
 const bodyParser = require('body-parser');
 const engines = require('consolidate');
 const cors = require('cors');
@@ -10,16 +9,12 @@ const app = express();
 redirect(app);
 
 
-
-app.use(cors());
 require('./startup/logging');
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config');
 require('./startup/prod')(app);
-app.use(express.static(__dirname + '/public'));
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+
 
 startupDebugger('first start');
 
--- routes/paytm/paytm.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const paytm = require('./controller/paytm.controller');
-
-router.get('/request', paytm.getRequest);
-router.post('/request', paytm.request);
-router.post('/response', paytm.response);
-
-
-module.exports = router;
