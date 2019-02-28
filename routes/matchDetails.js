27d72dc392b0cf6859dad9c795bfe9041079a080
Abhishek Singh <abhishek8938@gmail.com>
2019-02-28 22:01:36 +0530

    walletBalance 2nd commit

--- routes/matchDetails.js
+++ routes/matchDetails.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const matchdetails = await MatchDetail.find({matchStatus: req.params.matchStatus});
+  const matchdetails = await MatchDetail.find({matchStatus: req.body.matchStatus});
   console.log(matchdetails);
   res.send(matchdetails);
 });
