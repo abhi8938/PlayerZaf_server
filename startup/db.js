a862006990c3f1fb3fe5aa4c8929e45b7e11e81e
playerzaf <pllayerzaf.contact@gmail.com>
2019-03-24 20:56:14 +0530

    final mongodb playerzaf

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 module.exports = function () {
-mongoose.connect('mongodb+srv://Abhishek:abhishek@cluster1-h7cgu.mongodb.net/pubgc-db?retryWrites=true')
+//     mongoose.connect('mongodb+srv://Abhishek:abhishek@cluster1-h7cgu.mongodb.net/pubgc-db?retryWrites=true')
+// .then(() => console.log('connected to mongodb....'))
+// .catch(err => console.error('could not connect', err));
+
+mongoose.connect('mongodb+srv://playerzaf:12974711@cluster0-t59xr.mongodb.net/test?retryWrites=true')
 .then(() => console.log('connected to mongodb....'))
 .catch(err => console.error('could not connect', err));
 
