9f29cb2a94c16b9f8f859dadd0c24cd49fecc77f
PlayerZaf <playerzaf.contact@gmail.com>
2019-04-15 18:36:24 +0530

    14/04/2019

--- routes/results.js
+++ routes/results.js
@@ -16,7 +16,7 @@ router.get('/', auth, async (req, res, next) => {
   res.send(results);
 
 });
-router.post('/', [auth, admin], async (req, res) => {
+router.post('/', async (req, res) => {
   // console.log(req.body);
   const { error } = validate(req);
   if (error) return res.status(400).send(error.details[0].message);
